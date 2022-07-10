a3ca5ff552df8f972247aa3fd5cb268c516c4d1f
ilorwork <ilorwork64@gmail.com>
2022-07-10 14:46:06 +0300

    Load configuration from a json file

---
+++ UserVerificator/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace UserVerificator
+{
+    internal class Configuration
+    {
+        public string botToken;
+        public string logChatId;
+
+        public static Configuration LoadConfiguration()
+        {
+            using (StreamReader r = new StreamReader("userVerificatorConfig.json"))
+            {
+                string json = r.ReadToEnd();
+                Configuration config = JsonConvert.DeserializeObject<Configuration>(json);
+
+                return config;
+            }
+        }
+    }
+}
